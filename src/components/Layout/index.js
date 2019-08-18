2f9352ec6e6e14523ffbe2c97941b5bf0d503cea
Shehan Disanayake <dmcshehan@outlook.com>
2019-08-18 21:53:31 +0530

    background added

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,14 +2,28 @@ import React from "react";
 import { Layout } from "antd";
 import Navbar from "./Navbar";
 import Footer from "./Footer/Footer";
+import backgroundImage from "../../resources/background.jpg";
+
 const { Content } = Layout;
 
 const index = ({ children }) => {
   return (
     <Layout>
       <Navbar />
-      <Content style={{ padding: "0 50px", marginTop: 94 }}>
-        <div style={{ background: "#fff", padding: 24, minHeight: 380 }}>
+      <Content
+        style={{
+          padding: "0 50px",
+          paddingTop: 94,
+          minHeight: "calc(100vh - 69px)",
+          backgroundImage: `url(${backgroundImage})`
+        }}
+      >
+        <div
+          style={{
+            padding: 50,
+            minHeight: 380
+          }}
+        >
           {children}
         </div>
       </Content>
